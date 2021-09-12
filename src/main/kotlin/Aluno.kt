90dd2a9dd11fb322e63806c5b3e767cba2c19520
Christiane Valeriano <87905921+Chrisvaleriano@users.noreply.github.com>
2021-09-12 09:45:27 -0300

    Update Aluno.kt

--- src/main/kotlin/Aluno.kt
+++ src/main/kotlin/Aluno.kt
@@ -1,4 +1,9 @@
 class Aluno (var nome:String, var sobreNome:String , var codigoAluno:Int )  {
+  
+    override fun equals(other: Any?): Boolean {
+        return (other is nome) && (other.codigoAluno == this.nome)
+    }
+
 
 
 
