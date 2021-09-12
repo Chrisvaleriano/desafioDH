611e7b6dc96c8b532db9ab1268f03ee618c96ec3
Christiane Valeriano <87905921+Chrisvaleriano@users.noreply.github.com>
2021-09-12 09:59:53 -0300

    Parte B

--- src/main/kotlin/Curso.kt
+++ src/main/kotlin/Curso.kt
@@ -4,6 +4,10 @@ class Curso(var nome: String, var codigoCurso: Int) {
         professorTitular: ProfessorTitular, professorAdjunto: ProfessorAdjunto,
         quantMaxAlunos: Int, listaAlunosMatriculados: MutableList<Aluno>
     ) : this(nome, codigoCurso)
+    
+    override fun equals(other: Any?): Boolean {
+        return (other is Curso && this.codigoCurso == other.codigoCurso)
+    }
 
 }
 
