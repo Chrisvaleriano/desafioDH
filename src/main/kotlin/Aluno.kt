57b580838a72f36ec7a41d9412f8da3affe96846
Christianevaleriano <cristiane.valeriano@gmail.com>
2021-09-12 14:07:35 -0300

    correção Parte A

--- src/main/kotlin/Aluno.kt
+++ src/main/kotlin/Aluno.kt
@@ -2,10 +2,7 @@ class Aluno (var nomeAluno:String, var sobreNome:String , var codigoAluno:Int ) 
     override fun equals(other: Any?): Boolean {
         return (other is Aluno) && (other.codigoAluno == this.codigoAluno)
     }
-
     override fun hashCode(): Int {
         return codigoAluno
     }
-
-
     }
