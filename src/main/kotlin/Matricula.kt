97f1c487e917c36859f5e5add480092f3db91dfa
Christianevaleriano <cristiane.valeriano@gmail.com>
2021-09-13 14:32:15 -0300

    correção Parte F

--- src/main/kotlin/Matricula.kt
+++ src/main/kotlin/Matricula.kt
@@ -1,6 +1,6 @@
 import java.util.*
 
-class Matricula (var alunoA: Aluno, var cursoA: Curso){
-var data= Date()
+class Matricula (var alunoNovo: Aluno, var cursoA: Curso){
+var dataMatricula= Date()
 
  }
